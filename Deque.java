f9ce95928d6e77e713659ae82d202957e58dd6a6
Krishnavamsi <7krishnavamsi@msitprogram.net>
2022-03-14 14:27:55 +0530

    firstnode

--- Deque.java
+++ Deque.java
@@ -28,6 +28,13 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item)
+    {
+        Node second=first;
+        first= new Node();
+        first.item=item;
+        first.next=second;
+        n++;
+    }
 
     // add the item to the back
     public void addLast(Item item)
